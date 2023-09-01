9c1077fa426135696bbeb8f5acb50c5ca2a68bc7
Saglara <nohaevass@yandex.ru>
2023-09-01 20:27:02 +0300

    1

--- ex2.py
+++ ex2.py
@@ -5,3 +5,22 @@
 # Напишите аналогичный декоратор, но внутри используйте
 # модуль logging.
 
+import logging
+
+logging.basicConfig(level=logging.INFO, filename='loger.log', filemode='a',
+                    format='%(asctime)s, %(levelname)s, %(message)s')
+
+x, y = map(int, input('Введите два целых числа через пробел: ').split())
+def log_deco(func):
+    def wrapper(x, y):
+        try:
+            logging.info(f'Result: {x} / {y} = {func(x, y)}')
+        except:
+            logging.error('ZeroDivisionZero')
+    return wrapper
+
+@log_deco
+def log_div(x, y):
+    return (x / y)
+
+log_div(x, y)
